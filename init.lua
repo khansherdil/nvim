9612d208ec10fc85a98d1365239cf4f983c175eb
Sherdil <khan.sherdil@hotmail.com>
2022-12-26 20:14:59 +0100

    Testing GH

--- init.lua
+++ init.lua
@@ -27,6 +27,11 @@ require('packer').startup(function(use)
     },
   }
 
+  -- Tokyonight
+  use {
+    'folke/tokyonight.nvim'
+  }
+
   use { -- Autocompletion
     'hrsh7th/nvim-cmp',
     requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
@@ -464,3 +469,5 @@ vim.keymap.set("v", "<", "<gv", {})
 vim.keymap.set("n", "<leader>n", ":bnext<CR>", {})
 vim.keymap.set("n", "<leader>v", ":bprevious<CR>", {})
 
+
+-- Hi
